c61d5e432a503734ec379f44b7d7dc76fc8acd1c
arispap78 <73643527+arispap78@users.noreply.github.com>
2021-06-22 16:33:02 +0300

    descriptions of the tasks

--- src/Lesson4/MissingInteger.java
+++ src/Lesson4/MissingInteger.java
@@ -1,13 +1,26 @@
 package Lesson4;
 
 /**
- *
- * @author arispap78
- * task score 100%
- * correctness 100%
- * performance 100%
+ *@author arispap78
+ *task score 100%
+ *correctness 100%
+ *performance 100%
  */
+//<editor-fold>
+/**
+Write a function:class Solution { public int solution(int[] A); }
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) 
+that does not occur in A.
 
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+Given A = [1, 2, 3], the function should return 4.
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+//</editor-fold>
 import java.util.Arrays;
 import java.util.HashSet;
 
